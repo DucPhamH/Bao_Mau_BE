f13c2fe77057f9368f2404f7fbd0a9f2641d7d43
DucPhamH <ducphamhong2@gmail.com>
2023-05-27 21:45:06 +0700

    fix db

--- models/userModel.js
+++ models/userModel.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema(
   {
-    email: { type: String, maxlength: 160, default: "" },
-    name: { type: String, maxlength: 160, default: "" },
+    email: { type: String, minlength: 5, maxlength: 160 },
+    name: { type: String, maxlength: 160 },
     password: { type: String, required: true, minlength: 6, maxlength: 160 },
-    address: { type: String, maxlength: 160, default: "" },
+    address: { type: String, maxlength: 160 },
     phone: { type: String, maxlength: 20, require: true },
     roles: { type: Number, require: true },
     image: {
