f6cbfca12164c1a49527d2080cf806fcf26a8382
DucPhamH <ducphamhong2@gmail.com>
2023-06-11 16:01:05 +0700

    add api create v2

--- controllers/requestControllers.js
+++ controllers/requestControllers.js
@@ -1,5 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const RequestModel = require("../models/requestModel");
+const APIFeatures = require("../middleware/apiFeatures");
+const EmployeeModel = require("../models/employeeModel");
 
 const createRequest = asyncHandler(async (req, res, next) => {
   // const { _id } = req.user;
@@ -32,10 +34,55 @@ const createRequest = asyncHandler(async (req, res, next) => {
   }
 });
 
+const createRequest2 = asyncHandler(async (req, res, next) => {
+  const { _id } = req.user;
+  const { postID } = req.body;
+
+  const getEmployee = await EmployeeModel.findOne({ userID: _id });
+
+  if (getEmployee) {
+    console.log(getEmployee._id);
+    const request = await RequestModel.findOne({
+      postID: postID,
+      employeeID: getEmployee._id,
+    });
+
+    if (request) {
+      res
+        .status(400)
+        .json({ message: "gửi request thất bại, bạn đã gửi request này rồi" });
+      throw new Error("Lấy thất bại");
+    } else {
+      const newRequest = await RequestModel.create({
+        postID: postID,
+        employeeID: getEmployee._id,
+      });
+      if (newRequest) {
+        res
+          .status(200)
+          .json({ message: "gửi request thành công", data: newRequest });
+      } else {
+        res.status(400).json({ message: "gửi request thất bại" });
+        throw new Error("Lấy thất bại");
+      }
+    }
+  } else {
+    res.status(400).json({ message: "gửi request thất bại" });
+    throw new Error("Lấy thất bại");
+  }
+});
+
 const getAllRequest = asyncHandler(async (req, res, next) => {
   const request = await RequestModel.find({})
     .populate("employeeID")
     .populate("postID");
+
+  // const features = new APIFeatures(
+  //   RequestModel.find({}).populate("employeeID").populate("postID"),
+  //   req.query
+  // ).sort2();
+
+  // const request = await features.query;
 
   if (request) {
     res.status(200).json({ message: "gửi request thành công", data: request });
@@ -45,4 +92,4 @@ const getAllRequest = asyncHandler(async (req, res, next) => {
   }
 });
 
-module.exports = { createRequest, getAllRequest };
+module.exports = { createRequest, getAllRequest, createRequest2 };
--- routers/requestRoutes.js
+++ routers/requestRoutes.js
@@ -4,9 +4,11 @@ const checkRoleUser = require("../middleware/checkRoleUser");
 const {
   createRequest,
   getAllRequest,
+  createRequest2,
 } = require("../controllers/requestControllers");
 
 const router = express.Router();
 router.post("/createRequest", validateToken, createRequest);
 router.get("/", getAllRequest);
+router.post("/createRequest2", validateToken, createRequest2);
 module.exports = router;
